8067f20e4ed0dc83420c8273e9792d8524abed2e
Mike Wilson <mike1234@gmail.com>
2015-03-05 23:16:47 -0800

    Add divider content page

---
+++ app/controllers/divider.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    messages: [{
+        face: 'images/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+    }, {
+        face: 'images/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+    }, {
+        face: 'images/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+    }, {
+        face: 'images/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+    }, {
+        face: 'images/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+    }]
+});
