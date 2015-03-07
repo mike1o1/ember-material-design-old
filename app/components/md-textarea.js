17f24d2db904a2e9e19387bc9e97da0499123ea2
Mike Wilson <mike1234@gmail.com>
2015-03-06 21:03:31 -0800

    Small fix for textarea, so they autosize and input container gets correct class if textarea has a value.

--- app/components/md-textarea.js
+++ app/components/md-textarea.js
@@ -9,7 +9,18 @@ var MdTextArea = Ember.TextArea.extend({
 
         this.get('inputContainer').set('isFocused', false);
         this.get('inputContainer').set('hasValue', false);
+
+
     }.on('willDestroyElement'),
+
+    setupTextArea: function() {
+        if (this.get('value')) {
+            this.processInput();
+        }
+
+
+        this.changeTextArea();
+    }.on('didInsertElement'),
 
 
     setFocused: function(ev) {
@@ -21,7 +32,29 @@ var MdTextArea = Ember.TextArea.extend({
         var hasValue = this.get('value').length > 0;
         this.get('inputContainer').set('hasValue', hasValue);
 
-    }.on('input')
+    }.on('input'),
+
+    changeTextArea: function() {
+        var node = this.$()[0];
+        node.style.height = 'auto';
+        node.scrollTop = 0;
+        var height = this.getHeight(node);
+        if (height) {
+            node.style.height = height + 'px';
+        }
+    }.on('keydown input'),
+
+    getHeight: function(node) {
+        var line = node.scrollHeight - node.offsetHeight;
+        return node.offsetHeight + (line > 0 ? line : 0);
+    },
+
+    scroll: function() {
+        var node = this.$()[0];
+        var line = node.scrollheight - node.offsetHeight;
+        var height = node.offsetHeight + line;
+        node.style.height = height + 'px';
+    }
 });
 
 export default MdTextArea;
