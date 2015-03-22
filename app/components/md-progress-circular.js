922af0c73330c88fe26e9a756839a4a06538d238
Mike Wilson <mike1234@gmail.com>
2015-03-21 23:06:07 -0700

    remove dead code from progress-circular

--- app/components/md-progress-circular.js
+++ app/components/md-progress-circular.js
@@ -17,45 +17,16 @@ var MdProgressCircular = Ember.Component.extend({
     }.property('mdDiameter'),
 
     clampedValue: function() {
+
         var value = this.get('value');
-        if (value > 100) {
-            return 100;
-        }
 
-        if (value < 0) {
-            return 0;
-        }
-
-        return Math.ceil(value || 0);
+        return Math.max(0, Math.min(value || 0, 100));
+        
     }.property('value'),
-
-    fill: function() {
-        this.$()
-    },
 
     circleStyle: function() {
         return this.get('constants.CSS.TRANSFORM') + ': ' + 'scale(' + this.get('scale').toString() + ')';
-    }.property('scale'),
-
-    fillStyle: function() {
-        return this.get('constants.CSS.TRANSFORM') + ': ' + this.fillRotations(this.get('clampvedValue'));
-    }.property('clampedValue'),
-
-    fixStyle: function() {
-        return this.get('constants.CSS.TRANSFORM') + ': ' + this.fixRotations[this.get('clampedValue')];
-    }.property('clampedValue'),
-
-
-
-    setupRotations: function() {
-        for (var i = 0; i < 101; i++) {
-            var percent = i / 100;
-            var rotation = Math.floor(percent * 180);
-
-            this.fillRotations[i] = 'rotate(' + rotation.toString() + 'deg)';
-            this.fixRotations[i] = 'rotate(' + (rotation * 2).toString() + 'deg)';
-        }
-    }.on('didInsertElement')
+    }.property('scale')
 
 });
 
